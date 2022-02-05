1f362e7f9562ea7ad6af8974094baa48489e4044
Sarthak-22 <sarthakvora22@gmail.com>
2022-02-05 15:22:49 +0530

    Updated dataset.py

--- dataset.py
+++ dataset.py
@@ -5,8 +5,15 @@ from PIL import Image
 import numpy as np
 import os
 
+
+processing = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Resize((420,540)),
+                transforms.ToTensor(),
+            ])
+
 class NoisyDataset(Dataset):
-    def __init__(self, image_dir, label_dir, transform=None):
+    def __init__(self, image_dir, label_dir, transform=processing):
         self.image_dir = image_dir
         self.label_dir = label_dir
         self.transform = transform
@@ -31,18 +38,15 @@ class NoisyDataset(Dataset):
 
 
 
-processing = transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.Resize((420,540)),
-                transforms.ToTensor(),
-            ])
+
+def test():
+    test_dataset = NoisyDataset(image_dir='dataset/train', label_dir='dataset/train_cleaned', transform=processing)
+    test_image, test_label = test_dataset[0][0], test_dataset[0][1]         # Index = 0 
+    
+
+    plt.figure()
+    plt.imshow((test_image*255).numpy().transpose(1,2,0).astype(np.uint8))
+    plt.show()
 
 
-
-test_dataset = NoisyDataset(image_dir='dataset/train', label_dir='dataset/train_cleaned', transform=processing)
-test_image, test_label = test_dataset[0][0], test_dataset[0][1]         # Index = 0 
-print(test_image.max(), test_image.min())
-plt.figure()
-plt.imshow((test_image*255).numpy().transpose(1,2,0).astype(np.uint16))
-plt.show()
-
+test()
