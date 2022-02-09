03d27498770fe274d891b8879c9eaa57845ab4ae
Sarthak-22 <sarthakvora22@gmail.com>
2022-02-10 01:52:52 +0530

    added evaluation

--- eval.py
+++ eval.py
@@ -1,6 +1,32 @@
 import torch
+import sys
+from torchvision import transforms
+from PIL import Image
+import numpy as np
 from model import Conv_AE
-from dataset import NoisyDataset
 
+
+if (len(sys.argv>2)):
+    try:
+        img_path = sys.argv[2]
+    except:
+        assert('Enter correct file path')
+        exit
   
+  
+# Pre-processing the input image  
+def pre_processing(img_path):
+    image = np.array(Image.open(img_path), dtype=np.float32)/255.0
+
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((420,540)),
+        transforms.ToTensor(),
+    ])
+
+    processed_img = transform(image)
+
+
+# Passing the procesed input to the trained model
 model = Conv_AE()
+model.load_state_dict(torch.load())
